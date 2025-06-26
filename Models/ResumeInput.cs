be5642f35635dda9bd8205b8eaeb1a1eb6a5e093
Fernando Althann <159071708+FernandVonAlthann@users.noreply.github.com>
2025-06-26 01:03:51 -0700

    Add files via upload

---
+++ Models/ResumeInput.cs
@@ -0,0 +1,9 @@
+// Models/ResumeInput.cs
+namespace ResumeScreenerApp.Models
+{
+    public class ResumeInput
+    {
+        public string ResumeText { get; set; }
+        public string JobDescription { get; set; }
+    }
+}
